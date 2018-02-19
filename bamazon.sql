cd0b046bc597e725d94fd4be3d49061a6de6df9a
Nathan <nathanalston@gmail.com>
2018-02-19 18:44:52 -0500

    sql update

--- bamazon.sql
+++ bamazon.sql
@@ -5,9 +5,9 @@ CREATE DATABASE bamazon;
 -- Access Database --
 USE bamazon;
 
--- Create product table
+-- Create product table --
 CREATE TABLE products (
-  item_id INT(4) NOT NULL,
+  item_id INT AUTO_INCREMENT(4) NOT NULL,
   product_name VARCHAR(75) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
@@ -15,4 +15,11 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
+-- Ten Original Products --
 INSERT INTO products
+(product_name, department_name, price, stock_quantity)
+VALUES
+("Military Grade Gas Mask", "Prepper Supplies", 40, 60),
+("Hazmat Suit", "Prepper Supplies", 100, 75),
+("Fear and Loathing in Las Vegas", "Books", 16, 100),
+("Ready Player One", )
